b01658c9dd7ab19c1fc24e15d87f03e824d09a3d
tpipernob <tiagopiperno@gmail.com>
2018-09-24 09:28:16 -0300

    Criando a página do post

--- Blog/nossoBlog/urls.py
+++ Blog/nossoBlog/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', postagem, name='home'),
+    path('post/<int:pk>', postagem_list, name='post'),
 ]
--- Blog/nossoBlog/views.py
+++ Blog/nossoBlog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 
 def postagem(request, template_name='index.html'):
     postagem = Postagem.objects.all()
     postagens = {'lista': postagem}
     return render(request, template_name, postagens)
+
+def postagem_list(request, pk, template_name='post.html'):
+    post = get_object_or_404(Postagem, pk=pk)
+    return render(request, template_name, {'post': post})
